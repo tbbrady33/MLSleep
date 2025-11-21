867a44b2f9cc1ed3991713823e8e62ee02f7a348
Kaitlyn Brady <kmbrady@mac.lan>
2025-11-21 09:01:19 -0700

    made a dictionary

--- Get_sleep_data.py
+++ Get_sleep_data.py
@@ -2,22 +2,21 @@ class data:
     def get_data():
 
         sleep_data = {}
-        with open("Sleep_health_and_lifestyle_dataset.csv", "r") as file:
+        with open("Health_Sleep_Statistics", "r") as file:
             for line in file:
                 line = line.strip()
-                person_id, gender, age, occupation, sleep_duration, quality_of_sleep, physical_activity_level, stress_level, bmi_category, blood_pressure, heart_rate, daily_steps, sleep_disorder = line.split(",")
-                    
-                sleep_data[person_id] = {
+                user_id, age, gender, sleep_quality, bedtime, wakeup_time, daily_steps, calories_burned, physical_activity_level, dietary_habits, sleep_disorders, medication_usage = line.split(",")
+                
+                sleep_data[user_id] = {
+                    "age": age,
                     "gender": gender,
-                    "age": int(age),
-                    "occupation": occupation,
-                    "sleep duration": sleep_duration,
-                    "quality of sleep": quality_of_sleep, 
-                    "physical activity level": physical_activity_level, 
-                    "stress level": stress_level, 
-                    "bmi category": bmi_category, 
-                    "blood pressure": blood_pressure, 
-                    "heart rate": heart_rate, 
-                    "daily steps": daily_steps, 
-                    "sleep disorder": sleep_disorder
-                    }
+                    "sleep quality": sleep_disorders,
+                    "bedtime": bedtime,
+                    "wake-up time": wakeup_time,
+                    "daily steps": daily_steps,
+                    "calories burned": calories_burned,
+                    "physical activity level": physical_activity_level,
+                    "dietary habits": dietary_habits,
+                    "sleep disorders": sleep_disorders,
+                    "medication usage": medication_usage
+                }
