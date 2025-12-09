87c596d667fd395ece2374b329b26e0157a7dd5d
Kaitlyn Brady <kmbrady@Kaitlyns-MacBook-Air-329.local>
2025-12-08 20:35:18 -0700

    I made a dictgit add .

--- Get_sleep_data.py
+++ Get_sleep_data.py
@@ -1,5 +1,44 @@
+import csv
+
 class data:
     def get_data():
 
         sleep_data = []
-        
+        with open("/Users/kmbrady/Desktop/cs111/MLSleep/Health_Sleep_Statistics.csv", newline="") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                sleep_data.append(row)
+        return sleep_data
+
+Sleep_data = {
+    "Age": [],
+    "Gender": [],
+    "Sleep Quality": [],
+    "Bedtime": [],
+    "Wake-up Time": [],
+    "Daily Step": [],
+    "Calories Burned": [],
+    "Physical Activity Level": [],
+    "Dietary Habits": [],
+    "Sleep Disorders": [],
+    "Medication Usage": []
+
+}
+with open("/Users/kmbrady/Desktop/cs111/MLSleep/Health_Sleep_Statistics.csv", newline="") as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        Sleep_data["Age"].append(row[1])
+        Sleep_data["Gender"].append(row[2])
+        Sleep_data["Sleep Quality"].append(row[3])
+        Sleep_data["Bedtime"].append(row[4])
+        Sleep_data["Wake-up Time"].append(row[5])
+        Sleep_data["Daily Step"].append(row[6])
+        Sleep_data["Calories Burned"].append(row[7])
+        Sleep_data["Physical Activity Level"].append(row[8])
+        Sleep_data["Dietary Habits"].append(row[9])
+        Sleep_data["Sleep Disorders"].append(row[10])
+        Sleep_data["Medication Usage"].append(row[11])
+
+
+
--- MLAlgorythm.py
+++ MLAlgorythm.py
@@ -6,14 +6,11 @@ from Get_sleep_data import data
 
 sdata = data.get_data()
 
-
 num_samples = len(sdata)
 num_dep_variables = 11
 
-
 # 2. Split Data into Training and Testing Sets
 data_train, data_test= train_test_split(sdata, test_size=0.1, random_state=42)
-
 
 # 3. Create and Train the Linear Regression Model
 model = LinearRegression()
@@ -23,13 +20,11 @@ model.fit(data_train[:5] + data_train[5 + 1:], data_train[5])
 # 4. Make Predictions
 y_pred = model.predict(data_test[:5]+ data_test[5 + 1:])
 
-
 # 5. Evaluate the Model (Optional)
 # You can check the R-squared score or other metrics
 from sklearn.metrics import r2_score
 r_squared = r2_score(data_test[5], y_pred)
 print(f"R-squared score: {r_squared:.2f}")
-
 
 # Print the learned coefficients
 print(f"Intercept (b0): {model.intercept_[0]:.2f}")
